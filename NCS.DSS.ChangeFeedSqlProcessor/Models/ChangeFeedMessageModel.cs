f77ab8f99120cb0f3a0f8204944aadb63f944279
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-04-25 09:09:52 +0100

    Add remaining resources to change feed processor

--- NCS.DSS.ChangeFeedSqlProcessor/Models/ChangeFeedMessageModel.cs
+++ NCS.DSS.ChangeFeedSqlProcessor/Models/ChangeFeedMessageModel.cs
@@ -10,9 +10,14 @@ namespace NCS.DSS.ChangeFeedSqlProcessor.Models
         public bool IsActionPlan { get; set; }
         public bool IsAddress { get; set; }
         public bool IsAdviserDetail { get; set; }
+        public bool IsContact { get; set; }
         public bool IsCustomer { get; set; }
+        public bool IsDiversity { get; set; }
         public bool IsGoal { get; set; }
+        public bool IsInteraction { get; set; }
         public bool IsOutcome { get; set; }
         public bool IsSession { get; set; }
+        public bool IsTransfer { get; set; }
+        public bool IsWebChat { get; set; }
     }
 }
