024a027eae93e5e30eb2e34865e94f31c0c5ab74
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2024-08-08 11:47:55 +0100

    Upgrade to .NET8

--- NCS.DSS.ChangeFeedSqlProcessor/Processor/ChangeFeedQueueProcessor.cs
+++ NCS.DSS.ChangeFeedSqlProcessor/Processor/ChangeFeedQueueProcessor.cs
@@ -1,47 +1,43 @@
 using System;
-using DFC.Common.Standard.Logging;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using NCS.DSS.ChangeFeedSqlProcessor.Models;
 using NCS.DSS.ChangeFeedSqlProcessor.Service;
+using Microsoft.Azure.Functions.Worker;
 
 
 namespace NCS.DSS.ChangeFeedSqlProcessor.Processor
 {
     public class ChangeFeedQueueProcessor
     {
-        private readonly IChangeFeedQueueProcessorService _changeFeedQueueProcessorService;
-        private readonly ILoggerHelper loggerHelper;
+        private readonly IChangeFeedQueueProcessorService _changeFeedQueueProcessorService;        
+        private readonly ILogger _logger;
 
-        public ChangeFeedQueueProcessor(IChangeFeedQueueProcessorService changeFeedQueueProcessorService,
-            ILoggerHelper _loggerHelper)
+        public ChangeFeedQueueProcessor(IChangeFeedQueueProcessorService changeFeedQueueProcessorService,            
+            ILogger<ChangeFeedQueueProcessor> logger)
         {
-            _changeFeedQueueProcessorService = changeFeedQueueProcessorService;
-            loggerHelper = _loggerHelper;
+            _changeFeedQueueProcessorService = changeFeedQueueProcessorService;            
+            _logger = logger;
         }
 
-        [FunctionName("ChangeFeedQueueProcessor")]
+        [Function("ChangeFeedQueueProcessor")]
         public async System.Threading.Tasks.Task RunAsync(
-            [ServiceBusTrigger("%QueueName%", Connection = "ServiceBusConnectionString")] ChangeFeedMessageModel message, 
-            ILogger log)
+            [ServiceBusTrigger("%QueueName%", Connection = "ServiceBusConnectionString")] ChangeFeedMessageModel message)
         {
-
             var correlationId = Guid.NewGuid();
             if (message == null)
-            {
-                loggerHelper.LogInformationMessage(log, correlationId, "Brokered Message cannot be null");
+            {                
+                _logger.LogInformation($"CorrelationId: {correlationId} Message: Brokered Message cannot be null");
                 return;
             }
 
             try
             {
                 _changeFeedQueueProcessorService.CorrelationId = correlationId;
-                await _changeFeedQueueProcessorService.SendToAzureSql(message, log);
+                await _changeFeedQueueProcessorService.SendToAzureSql(message, _logger);
             }
             catch (Exception ex)
-            {
-                loggerHelper.LogException(log, correlationId, "Unable to send document to sql", ex);
+            {                
+                _logger.LogError("CorrelationId: {0}  Message: {1} Exception: {2}", correlationId, "Unable to send document to sql", ex);
                 throw;
             }
 
